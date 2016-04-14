8ddf672e57a6c1a31fd6088090af0eacf27e4333
Jerhone <fjcamillo.dev@gmail.com>
2016-04-14 23:56:00 +0800

    Main Code
    Main Code that runs

---
+++ Darwin'sTheory.py
@@ -0,0 +1,18 @@
+try:
+    userInput = raw_input("Type a number that you want to reduce to 1: ")
+    tracker = 0
+    while(int(userInput)!=1):
+        if (int(userInput)%2==0):
+            userInput = int(userInput)/2
+            print userInput
+            tracker += 1
+        elif(int(userInput)%2!=0):
+            userInput = int(userInput)*3+1
+            print userInput
+            tracker += 1
+
+    print "The Answer is now finished"
+    print tracker
+
+except:
+    print "You typed something wrong"
